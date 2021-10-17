d4a1c02cb7d8c0fb649141559ff0cb4afafe90d6
Rodrigo Sanabria <krosf@krosf.local>
2021-10-17 15:02:30 +0200

    feat: add babel-import-resolver

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ module.exports = {
       '@typescript-eslint/parser': ['.ts', '.tsx'],
     },
     'import/resolver': {
+      'babel-module': {},
       typescript: {
         alwaysTryTypes: true,
       },
